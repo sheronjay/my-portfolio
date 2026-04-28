91136e25c91b1a4a1e086188f864d9ac9a3b77e4
sheronjay <192441684+sheronjay@users.noreply.github.com>
2026-04-28 21:41:53 +0530

    migrate to astro

---
+++ agreeable-accretion/src/consts.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = 'Sheron Jayasinghe';
+export const SITE_DESCRIPTION =
+	'Portfolio website of Sheron Jayasinghe, a Computer Science & Engineering undergraduate focused on robotics, IoT, and integrated computer engineering.';
